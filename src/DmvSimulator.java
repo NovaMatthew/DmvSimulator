2ba6219f372b58e7b9685c327994b5e23d3e7926
NovaMatthew <mnova412@gmail.com>
2022-03-06 13:25:36 -0500

    setup

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,9 +1,43 @@
+import java.util.Random;
 
 public class DmvSimulator {
 
 	public static void main(String[] args) {
+	
+		System.out.println("Hello user welcome to the DMV Simulator.");
+
+		Random rand = new Random();
 		
+		
+		int userNum = rand.nextInt(101);
+		
+		//int userNum = 10;
+		
+		System.out.println("Here is your number: " + userNum);
+		System.out.println("Please wait until your number is called.");
+		
+		int count = userNum;
+		
+		
+		boolean yourTurn = false;
 
+			while(!yourTurn)
+			{
+				count = count + 1;
+				System.out.println("Next number is: " + count);
+			
+			if (count == 100)
+			{
+				count = 0;
+			}
+			if (count == userNum)
+			{
+				System.out.println("Sorry, you do not have the required paperwork.");
+				yourTurn = true;
+			}
+		}
 	}
-
+		
 }
+
+	
